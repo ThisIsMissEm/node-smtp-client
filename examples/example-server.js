7826ab67275c6120862d224d4516797249b1603e
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2010-01-16 23:27:40 +0800

    Initial rough implementation of an SMTP server

--- examples/example-server.js
+++ examples/example-server.js
@@ -1,14 +1,15 @@
 /*===============================================
 	File:      example-server.js
-	
+
 	Author:    Micheil Smith
 	Description:
 		An example of the node-smtp server.
-		
+
 		(Not Implemented)
 ===============================================*/
 
-
 var smtp = require("../lib/smtp");
 
-var server = smtp.Server();
+var server = new smtp.Server();
+server.runServer();
+
