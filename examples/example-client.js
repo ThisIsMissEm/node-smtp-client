1de711ac166c7d6b371238a9e2aa6f1a8b642214
Micheil Smith <micheil@brandedcode.com>
2010-01-17 04:55:49 +1100

    Fully converted to using Promises for SMTP commands.

--- examples/example-client.js
+++ examples/example-client.js
@@ -11,8 +11,20 @@ var smtp = require("../lib/smtp");
 
 var client = new smtp.Client();
 
+var message = "Date: "+(new Date()).toString()+" \
+From: Node-SMTP <"+config.from+">\
+Subject: Node SMTP Works!\
+To: micheil@brandedcode.com\
+So, It looks like node-smtp all works, which is great news! Time for some sleep I think though.\
+\
+G'night Micheil.";
+
 client.connect(config.port, config.host).addCallback(function(){
 	client.mail(config.from).addCallback(function(){
-		client.rcpt(config.to);
+		client.rcpt(config.to).addCallback(function(){
+			client.data(message).addCallback(function(){
+				client.quit();
+			});
+		});
 	});
 });
