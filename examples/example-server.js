96137a47b92e30f72b742072279fcbf7f12c4bc2
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2010-01-18 16:22:58 +0800

    Callback proof of concept

--- examples/example-server.js
+++ examples/example-server.js
@@ -13,3 +13,10 @@ var smtp = require("../lib/smtp");
 var server = new smtp.Server();
 server.runServer();
 
+var sys = require('sys');
+server.addListener('connect', function( args ) {
+  var remoteHost = args[0];
+  var promise = args[1];
+  sys.puts( 'connect callback' );
+  promise.emitError("I don't like your type [" + remoteHost +"]");
+});
